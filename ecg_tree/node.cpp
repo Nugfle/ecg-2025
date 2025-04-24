04694429b0015795d374d64071c394ee12b07678
Niklas Liesch <niklas.liesch@protonmail.com>
2025-04-24 16:22:35 +0200

    adding all methods 	modified:   node.cpp

--- ecg_tree/node.cpp
+++ ecg_tree/node.cpp
@@ -1,4 +1,12 @@
 #include "node.h"
 #include <string>
+#include <vector>
 
-Node::Node(const std::string &n) : name(n) {}
+Node::Node(const std::string &n) : name(n), children(std::vector<Node *>()) {}
+Node::~Node() {}
+
+std::string &Node::get_name() const {}
+void Node::set_name(std::string &new_name) {}
+int Node::get_nr_children() const {}
+Node *Node::get_child(int i) const {}
+void Node::add_child(Node child) {}
