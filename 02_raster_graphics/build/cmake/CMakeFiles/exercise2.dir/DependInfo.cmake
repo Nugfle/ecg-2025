88d578029ff3834c70f1024746aaa9c629a49e06
Niklas Liesch <niklas.liesch@protonmail.com>
2025-05-15 15:37:28 +0200

    adding cmake dir 	modified:   .gitignore 	new file:   02_raster_graphics/build/cmake/CMakeCache.txt 	new file:   02_raster_graphics/build/cmake/CMakeFiles/4.0.1/CMakeCCompiler.cmake 	new file:   02_raster_graphics/build/cmake/CMakeFiles/4.0.1/CMakeCXXCompiler.cmake 	new file:   02_raster_graphics/build/cmake/CMakeFiles/4.0.1/CMakeSystem.cmake 	new file:   02_raster_graphics/build/cmake/CMakeFiles/4.0.1/CompilerIdC/CMakeCCompilerId.c 	new file:   02_raster_graphics/build/cmake/CMakeFiles/4.0.1/CompilerIdC/a.out 	new file:   02_raster_graphics/build/cmake/CMakeFiles/4.0.1/CompilerIdCXX/CMakeCXXCompilerId.cpp 	new file:   02_raster_graphics/build/cmake/CMakeFiles/4.0.1/CompilerIdCXX/a.out 	new file:   02_raster_graphics/build/cmake/CMakeFiles/CMakeConfigureLog.yaml 	new file:   02_raster_graphics/build/cmake/CMakeFiles/CMakeDirectoryInformation.cmake 	new file:   02_raster_graphics/build/cmake/CMakeFiles/InstallScripts.json 	new file:   02_raster_graphics/build/cmake/CMakeFiles/Makefile.cmake 	new file:   02_raster_graphics/build/cmake/CMakeFiles/Makefile2 	new file:   02_raster_graphics/build/cmake/CMakeFiles/TargetDirectories.txt 	new file:   02_raster_graphics/build/cmake/CMakeFiles/cmake.check_cache 	new file:   02_raster_graphics/build/cmake/CMakeFiles/exercise2.dir/DependInfo.cmake 	new file:   02_raster_graphics/build/cmake/CMakeFiles/exercise2.dir/build.make 	new file:   02_raster_graphics/build/cmake/CMakeFiles/exercise2.dir/cmake_clean.cmake 	new file:   02_raster_graphics/build/cmake/CMakeFiles/exercise2.dir/compiler_depend.make 	new file:   02_raster_graphics/build/cmake/CMakeFiles/exercise2.dir/compiler_depend.ts 	new file:   02_raster_graphics/build/cmake/CMakeFiles/exercise2.dir/depend.make 	new file:   02_raster_graphics/build/cmake/CMakeFiles/exercise2.dir/flags.make 	new file:   02_raster_graphics/build/cmake/CMakeFiles/exercise2.dir/link.txt 	new file:   02_raster_graphics/build/cmake/CMakeFiles/exercise2.dir/progress.make 	new file:   02_raster_graphics/build/cmake/CMakeFiles/progress.marks 	new file:   02_raster_graphics/build/cmake/CMakeLists.txt 	new file:   02_raster_graphics/build/cmake/Makefile 	new file:   02_raster_graphics/build/cmake/cmake_install.cmake 	new file:   02_raster_graphics/build/cmake/exercise2 	new file:   02_raster_graphics/build/vs2012/raster_graphics.sln 	new file:   02_raster_graphics/build/vs2012/raster_graphics.vcxproj 	new file:   02_raster_graphics/build/vs2015/raster_graphics.sln 	new file:   02_raster_graphics/build/vs2015/raster_graphics.vcxproj 	new file:   02_raster_graphics/build/vs2017/raster_graphics.sln 	new file:   02_raster_graphics/build/vs2017/raster_graphics.vcxproj 	new file:   ecg_tree/src/readme.txt

---
+++ 02_raster_graphics/build/cmake/CMakeFiles/exercise2.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/niklas/Programs/ecg/02_raster_graphics/src/application.cpp" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/application.cpp.o" "gcc" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/application.cpp.o.d"
+  "/home/niklas/Programs/ecg/02_raster_graphics/src/bresenham_circle_tool.cpp" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/bresenham_circle_tool.cpp.o" "gcc" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/bresenham_circle_tool.cpp.o.d"
+  "/home/niklas/Programs/ecg/02_raster_graphics/src/bresenham_line_tool.cpp" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/bresenham_line_tool.cpp.o" "gcc" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/bresenham_line_tool.cpp.o.d"
+  "/home/niklas/Programs/ecg/02_raster_graphics/src/canvas_buffer.cpp" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/canvas_buffer.cpp.o" "gcc" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/canvas_buffer.cpp.o.d"
+  "/home/niklas/Programs/ecg/02_raster_graphics/src/canvas_renderer.cpp" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/canvas_renderer.cpp.o" "gcc" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/canvas_renderer.cpp.o.d"
+  "/home/niklas/Programs/ecg/02_raster_graphics/src/dda_line_tool.cpp" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/dda_line_tool.cpp.o" "gcc" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/dda_line_tool.cpp.o.d"
+  "/home/niklas/Programs/ecg/02_raster_graphics/src/line_fill_tool.cpp" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/line_fill_tool.cpp.o" "gcc" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/line_fill_tool.cpp.o.d"
+  "/home/niklas/Programs/ecg/02_raster_graphics/src/main.cpp" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/main.cpp.o" "gcc" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/main.cpp.o.d"
+  "/home/niklas/Programs/ecg/02_raster_graphics/src/non_recursive_fill_tool.cpp" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/non_recursive_fill_tool.cpp.o" "gcc" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/non_recursive_fill_tool.cpp.o.d"
+  "/home/niklas/Programs/ecg/02_raster_graphics/src/pen_tool.cpp" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/pen_tool.cpp.o" "gcc" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/pen_tool.cpp.o.d"
+  "/home/niklas/Programs/ecg/02_raster_graphics/src/preview_renderer.cpp" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/preview_renderer.cpp.o" "gcc" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/preview_renderer.cpp.o.d"
+  "/home/niklas/Programs/ecg/02_raster_graphics/src/rectangle_tool.cpp" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/rectangle_tool.cpp.o" "gcc" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/rectangle_tool.cpp.o.d"
+  "/home/niklas/Programs/ecg/02_raster_graphics/src/recursive_fill_tool.cpp" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/recursive_fill_tool.cpp.o" "gcc" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/recursive_fill_tool.cpp.o.d"
+  "/home/niklas/Programs/ecg/02_raster_graphics/src/tool_base.cpp" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/tool_base.cpp.o" "gcc" "CMakeFiles/exercise2.dir/home/niklas/Programs/ecg/02_raster_graphics/src/tool_base.cpp.o.d"
+  "" "exercise2" "gcc" "CMakeFiles/exercise2.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ecg_tree/src/readme.txt
@@ -0,0 +1,12 @@
+Niklas Liesch, 5212280
+Ole Peters, 5250085
+
+Zusatzaufgaben 1, 2 und 3.
+
+C++ 14.2.1
+
+How to run it:
+
+1. cd build
+2. cmake ..
+3. make 
